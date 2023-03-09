5df4aa7b7a94e4801dbc5568c9e8f6d7f8c01610
Shavin <shavinsingh1999@gmail.com>
2023-03-09 13:51:30 -0500

    Add project files.

---
+++ HospitalProjectStJoeseph/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalProjectStJoeseph.Startup))]
+namespace HospitalProjectStJoeseph
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
