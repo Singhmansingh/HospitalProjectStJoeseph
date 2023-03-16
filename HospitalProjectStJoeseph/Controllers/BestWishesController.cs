2f3b02f385764686a1a816edb64bdaad2e410034
Shavin <shavinsingh1999@gmail.com>
2023-03-16 14:27:47 -0400

    Added Patient And Best Wishes scaffold

---
+++ HospitalProjectStJoeseph/Controllers/BestWishesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalProjectStJoeseph.Controllers
+{
+    public class BestWishesController : Controller
+    {
+        // GET: BestWishes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
