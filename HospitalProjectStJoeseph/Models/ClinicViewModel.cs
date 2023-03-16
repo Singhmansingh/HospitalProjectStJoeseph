1874a9e7a77ffdbdcceb2b31b599bf0073a2803c
danya <danya@DESKTOP-EGSDHJL>
2023-03-16 14:28:37 -0400

    Clinic and service models

---
+++ HospitalProjectStJoeseph/Models/ClinicViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalProjectStJoeseph.Models
+{
+    public class ClinicViewModel : Controller
+    {
+        // GET: ClinicViewModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
