a992bdabd8505c73c1c3ec5c8d6248c0f9222cff
Frank Xie <zh.xie@alum.utoronto.ca>
2023-03-23 14:48:29 -0400

    created physician

---
+++ HospitalProjectStJoeseph/Models/Physician.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.Models
+{
+    public class Physician
+    {
+        [Key]
+        public int physician_id { get; set; }
+
+        public string physician_name { get; set; }
+
+        public string specialty { get; set; }
+
+        public string phone { get; set; }
+
+        public string email { get; set; }
+    }
+}
