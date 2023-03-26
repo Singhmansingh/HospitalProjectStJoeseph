3e848239fcae425bb5f4c43ec8506509be987428
Shavin <64917885+Singhmansingh@users.noreply.github.com>
2023-03-26 19:55:26 -0400

    Added Best Wishes & Patient basic CRUD API endpoints

--- HospitalProjectStJoeseph/Controllers/BestWishesDataController.cs
+++ HospitalProjectStJoeseph/Controllers/BestWishesDataController.cs
@@ -1,13 +1,107 @@
-﻿using System;
+﻿using HospitalProjectStJoeseph.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace HospitalProjectStJoeseph.Controllers
 {
     public class BestWishesDataController : ApiController
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        //TODO: List Best Wishes
+        [HttpGet]
+        [ResponseType(typeof(List<BestWish>))]
+        public IHttpActionResult List()
+        {
+            List<BestWish> bestWishes = db.BestWishes.ToList();
+
+            return Ok(bestWishes);
+        }
+
+        //TOD: List Best Wishes for Patient
+        [HttpGet]
+        [ResponseType(typeof(List<BestWish>))]
+        public IHttpActionResult ListFor(int PatientId)
+        {
+            List<BestWish> bestWishes = db.BestWishes.Where(bw => bw.PatientId == PatientId).ToList();
+
+            if(bestWishes.Count > 0)
+            {
+                return Ok(bestWishes);
+            }
+            return BadRequest();
+
+        }
+        //TODO: Add Best Wish
+        [HttpPost]
+        [Authorize]
+
+        public IHttpActionResult Add([FromBody] BestWish BestWish)
+        {
+            var res = db.BestWishes.Add(BestWish);
+            db.SaveChanges();
+            if (res != null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+
+            return BadRequest();
+
+        }
+
+        //TODO: Update Best Wish
+        [HttpPost]
+        [Authorize]
+
+        public IHttpActionResult UpdateBestWish(int id, [FromBody] BestWish BestWish)
+        {
+            if(!BestWishExists(id))
+            {
+                return NotFound();
+            }
+
+            db.BestWishes.AddOrUpdate(BestWish);
+
+            try
+            {
+                db.SaveChanges();
+
+            } catch
+            {
+                return BadRequest();
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+
+        }
+
+        //TODO: Delete Best Wish
+        [HttpPost]
+        [ResponseType(typeof(BestWish))]
+        [Authorize]
+
+        public IHttpActionResult DeleteBestWish(int id)
+        {
+            BestWish bestWish = db.BestWishes.Find(id);
+            if(bestWish == null)
+            {
+                return NotFound();
+            }
+            db.BestWishes.Remove(bestWish);
+            return Ok();
+        }
+
+
+        private bool BestWishExists(int id)
+        {
+            return db.BestWishes.Count(bw => bw.BestWishId == id) > 0;
+        }
     }
 }
--- HospitalProjectStJoeseph/Controllers/PatientDataController.cs
+++ HospitalProjectStJoeseph/Controllers/PatientDataController.cs
@@ -1,13 +1,93 @@
-﻿using System;
+﻿using HospitalProjectStJoeseph.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace HospitalProjectStJoeseph.Controllers
 {
     public class PatientDataController : ApiController
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        //TODO: List Patients
+        [HttpGet]
+        [ResponseType(typeof(List<Patient>))]
+        public IHttpActionResult List()
+        {
+            List<Patient> Patients = db.Patients.ToList();
+
+            return Ok(Patients);
+        }
+
+        //TODO: Add Patient
+        [HttpPost]
+        [Authorize]
+
+        public IHttpActionResult Add([FromBody] Patient Patient)
+        {
+            var res = db.Patients.Add(Patient);
+            db.SaveChanges();
+            if (res != null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+
+            return BadRequest();
+
+        }
+
+        //TODO: Update Patient
+        [HttpPost]
+        [Authorize]
+
+        public IHttpActionResult UpdatePatient(int id, [FromBody] Patient Patient)
+        {
+            if (!PatientExists(id))
+            {
+                return NotFound();
+            }
+
+            db.Patients.AddOrUpdate(Patient);
+
+            try
+            {
+                db.SaveChanges();
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+
+        }
+
+        //TODO: Delete Patient
+        [HttpPost]
+        [ResponseType(typeof(Patient))]
+        [Authorize]
+
+        public IHttpActionResult DeletePatient(int id)
+        {
+            Patient Patient = db.Patients.Find(id);
+            if (Patient == null)
+            {
+                return NotFound();
+            }
+            db.Patients.Remove(Patient);
+            return Ok();
+        }
+
+
+        private bool PatientExists(int id)
+        {
+            return db.Patients.Count(p => p.PatientId == id) > 0;
+        }
     }
 }
