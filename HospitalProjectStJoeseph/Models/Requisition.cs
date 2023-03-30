db9eb70df4959113c47476578ec9412fd422b85b
wenhao-lu <wenhaolu.kevin@gmail.com>
2023-03-30 01:22:20 -0400

    CRUD for "Requisition" and "Test"

--- HospitalProjectStJoeseph/Models/Requisition.cs
+++ HospitalProjectStJoeseph/Models/Requisition.cs
@@ -12,12 +12,9 @@ namespace HospitalProjectStJoeseph.Models
 
         [Key]
         public int RequisitionID { get; set; }
-        public int patient_id { get; set; }
-        public int physician_id { get; set; }
-        public int clinic_id { get; set; }
-        public int test_id { get; set; }
-        public DateTime record_date { get; set; }
         public string test_result { get; set; }
+        public DateTime record_date { get; set; }
+
 
 
         [ForeignKey("Patient")]
@@ -26,7 +23,7 @@ namespace HospitalProjectStJoeseph.Models
 
 
         [ForeignKey("Physician")]
-        public int PhysicianId { get; set; }
+        public int physician_id { get; set; }
         public virtual Physician Physician { get; set; }
 
         [ForeignKey("Clinic")]
@@ -39,21 +36,22 @@ namespace HospitalProjectStJoeseph.Models
 
 
 
-
-
-
-
-
     }
     public class RequisitionDto
     {
         public int RequisitionID { get; set; }
-        public int patient_id { get; set; }
-        public int physician_id { get; set; }
-        public int clinic_id { get; set; }
-        public int test_id { get; set; }
-        public DateTime record_date { get; set; }
         public string test_result { get; set; }
+        public DateTime record_date { get; set; }
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public int physician_id { get; set; }
+        public string physician_name { get; set; }
+        public int ClinicId { get; set; }
+        public string ClinicName { get; set; }
+        public int TestID { get; set; }
+        public string test_category { get; set; }
+        public DateTime test_date { get; set; }
+
 
     }
 
